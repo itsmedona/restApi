f176c146b5d50bdb0ba6573515490531be645e06
itsmedona <donashaji254@gmail.com>
2024-04-19 12:27:23 +0530

    prim;;

--- lib/model/User/User.dart
+++ lib/model/User/User.dart
@@ -4,9 +4,19 @@ class User {
   final String phone;
   final String cell;
   final String nat;
-  User({required this.gender, 
-  required this.email,
-  required this.phone,
-  required this.cell,
-  required this.nat});
+  final UserName name;
+  User(
+      {required this.gender,
+      required this.email,
+      required this.phone,
+      required this.cell,
+      required this.nat,
+      required this.name});
+}
+
+class UserName {
+  final String title;
+  final String first;
+  final String last;
+  UserName({required this.title, required this.first, required this.last});
 }
--- lib/view/HomePage/HomePage.dart
+++ lib/view/HomePage/HomePage.dart
@@ -24,14 +24,15 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           final user = users[index];
           // final name = user.name;
-          final email = user.email;
+          //final email = user.email;
+          final color = user.gender == 'male'
+              ? Colors.lightGreenAccent
+              : Colors.deepPurpleAccent;
           //final imageUrl = user['picture']['thumbnail'];
           return ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              //child: Image.network(imageUrl),
-            ),
-            title: Text(email),
+            title: Text(user.name.first),
+            subtitle: Text(user.phone),
+            tileColor: color,
             //subtitle: Text(email),
           );
         },
@@ -51,12 +52,17 @@ class _HomePageState extends State<HomePage> {
     final json = jsonDecode(body);
     final results = json['results'] as List<dynamic>;
     final transformed = results.map((e) {
+      final name = UserName(
+          title: e['name']['title'],
+          first: e['name']['first'],
+          last: e['name']['last']);
       return User(
           gender: e['gender'],
           email: e['email'],
           phone: e['phone'],
           cell: e['cell'],
-          nat: e['nat']);
+          nat: e['nat'],
+          name: name);
     }).toList();
     setState(() {
       users = transformed;
